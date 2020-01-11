bcd9df5688b88fb08ad72d831c59c0e1dac9dea9
Ewelina <38459295+Chimcia@users.noreply.github.com>
2020-01-11 21:20:50 +0100

    Add files via upload

---
+++ game_assets/support_stuff/tileA2_outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileA2_outside" tilewidth="24" tileheight="24" tilecount="768" columns="32">
+ <image source="Assets/tileA2_outside.png" trans="ff00ff" width="768" height="576"/>
+</tileset>
---
+++ game_assets/support_stuff/tileA4_dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileA4_dungeon" tilewidth="24" tileheight="24" tilecount="960" columns="32">
+ <image source="Assets/tileA4_dungeon.png" trans="ff00ff" width="768" height="720"/>
+</tileset>
